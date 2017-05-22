7e98ebd33049e14d1cde1eda1cccb7e8b75b0224
Anton_Solianyk <anton_solianyk@epam.com>
2017-05-22 18:10:50 +0300

    MOB-1: Commit idea project.

---
+++ mo-backend/src/main/java/mo/MOApplication.java
@@ -0,0 +1,12 @@
+package mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MOApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MOApplication.class, args);
+	}
+}
